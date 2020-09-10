798f80e0cf78d01976d183d33b79df78d69a6bed
Yuki-Ichigo <bzpl1215@gmail.com>
2020-09-10 12:54:05 +0000

    [ログイン機能追加]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+  	protected
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :name_kana, :nic_name, :sex, :birthday, :zip_code, :address_prefecture_name, :address_city, :address_line1, :address_line2, :phone_number, :password, :password_confirmation, :email])
+	end
 end
