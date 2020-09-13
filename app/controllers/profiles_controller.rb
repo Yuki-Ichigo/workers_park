25a084a95a60bceeb0289785f0a8f099c102e1c1
Yuki-Ichigo <bzpl1215@gmail.com>
2020-09-13 04:52:49 +0000

    [プロフィール新規フォーム追加]

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,40 @@
 class ProfilesController < ApplicationController
+
+	def create
+		@profile = Profile.new
+        @profile.user_id = current_user.id
+    	if  @profile.save
+        	redirect_to edit_profile_path(@profile.id), notice: "新規登録が完了しました"
+    	else
+        	@user = current_user(current_user.id)
+        	render 'users/show'
+    	end
+	end
+
+	def index
+		@profiles = Profile.all
+	end
+
+	def show
+		if Profile.find(params[:id]) == blank? 
+			@user = current_user
+			redirect_to new_profile_path(current_user.id)
+		else
+		    @profie = Profile.find(params[:id])
+		end
+	end
+
+	def edit
+	end
+
+	def updage
+	end
+
+    private
+
+    def profile_params
+    params.require(:profile).permit(:introduction, :future, :employment_type, :user_id)
+    end
+
+	
 end
