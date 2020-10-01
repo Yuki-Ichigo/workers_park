0eda2b63a601048c8767289b34ae4aaeb41149d6
Yuki-Ichigo <bzpl1215@gmail.com>
2020-10-01 11:17:54 +0000

    talkroom index 修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  has_one :profile, dependent: :destroy
+  has_one :profile, dependent: :delete
 
   has_many :company_members, dependent: :destroy
   has_many :companies, through: :company_members
